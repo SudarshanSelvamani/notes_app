8bc842af6d4eb54cfe3f0dfa5b2e4a570c527547
Sudarshan <sudarshan@testpress.in>
2021-09-25 19:00:58 +0530

    Add notes list view

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Note, Notebook
+
+
+class Mixin:
+    def create_notebook(self, name="English"):
+        return Notebook.objects.create(name=name)
+
+    def create_note(self, title="Chapter1", body="jigsaw", notebook=None):
+        if notebook == None:
+            notebook = self.create_notebook()
+        return Note.objects.create(title=title, body=body, notebook=notebook)
--- notes_app/settings.py
+++ notes_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
+    "notebooks",
     "notes",
     "taggit",
 ]
@@ -57,7 +59,7 @@ ROOT_URLCONF = "notes_app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -66,6 +68,9 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "libraries": {
+                "myfilters": "templatetags.myfilters",
+            },
         },
     },
 ]
@@ -121,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
