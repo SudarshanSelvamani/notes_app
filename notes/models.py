d099c6643b5fc7f9ddc751573061656cb8c9e177
SudarshanTestpress <86759435+SudarshanTestpress@users.noreply.github.com>
2021-09-28 10:46:13 +0530

    Add models for Note, Notebook(#1)

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from taggit.managers import TaggableManager
+from model_utils.models import TimeStampedModel
+
+
+class Notebook(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Note(TimeStampedModel):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    notebook = models.ForeignKey(
+        Notebook, blank=True, null=True, on_delete=models.CASCADE
+    )
+    tag = TaggableManager(blank=True)
+
+    def __str__(self):
+        return self.title
