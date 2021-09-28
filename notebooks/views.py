cbf5a23691ecf02a7917eb086087918c92c18f0e
Sudarshan <sudarshan@testpress.in>
2021-09-28 18:38:44 +0530

    remove notebook list view

--- notebooks/views.py
+++ notebooks/views.py
@@ -12,17 +12,6 @@ from .forms import NotebookForm
 
 
 @method_decorator(login_required, name="dispatch")
-class NotebookListView(ListView):
-    model = Notebook
-    paginate_by = 9
-    context_object_name = "notebooks"
-    template_name = "notes/notebook_list.html"
-
-    def get_queryset(self):
-        return Notebook.objects.annotate(Count("note"))
-
-
-@method_decorator(login_required, name="dispatch")
 class NoteListView(ListView):
     model = Note
     paginate_by = 6
