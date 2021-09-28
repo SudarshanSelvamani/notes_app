a783e04f9674f71a25b49f46e88e973f364d4209
Sudarshan <sudarshan@testpress.in>
2021-09-28 07:32:48 +0530

    Add update view for notes

--- notes/urls.py
+++ notes/urls.py
@@ -6,4 +6,7 @@ app_name = "notes"
 
 urlpatterns = [
     path("create", views.NoteCreateView.as_view(), name="create"),
+    path(
+        "<str:pk>/update/<str:note_pk>", views.NoteUpdateView.as_view(), name="update"
+    ),
 ]
